e04eaf938c45b5ae8bbe2b20633fe9130e9fa5d6
Gurkaran Singh <gurkaran1314@gmail.com>
2023-08-03 17:35:20 +0530

    feed section add

---
+++ client/src/components/Main/Feed/FeedElements.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  z-index: 50;
+  background-color: #dbe1e8;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0rem calc((100vw - 1300px) / 2);
+  padding: 2rem;
+`;
+
+export const PostWrapper = styled.div`
+    font-size: 18px;
+    padding: 1rem;
+    word-spacing: 6px;
+`;
+export const Starting = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+`
+export const Heading = styled.div`
+    font-size: 16px;
+    
+`
+export const ReadTime = styled.div`
+    
+`
+export const Title = styled.div`
+    font-size: 24px;
+    font-weight: bolder;
+`
+export const Description = styled.div`
+    
+`
+export const Ending = styled.div`
+    
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+`
+export const Likes = styled.div`
+    
+`
+export const Comments = styled.div`
+    
+`
+export const Date = styled.div`
+    
+`
--- client/src/components/Main/Feed/index.jsx
+++ client/src/components/Main/Feed/index.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { postArray } from "./Data";
-
+import {Wrapper, PostWrapper , Starting  ,Heading , ReadTime,  Title , Description , Ending , Likes , Comments , Date} from "./FeedElements";
 const Feed = () => {
   const navigate = useNavigate();
   const changeRoute = () => {
@@ -9,15 +9,31 @@ const Feed = () => {
   };
   return (
     <>
+        <Wrapper>
         {postArray.map((item) => {
-        return (
-          <li className="card p-3 mb-2">
-            <div className="card-body">
-              <p className="card-text">{item.title}</p>
-            </div>
-          </li>
-        );
-      })}
+            return (
+            <PostWrapper>
+            <hr />
+                <Starting>
+                    <Heading>{item.author}</Heading>
+                    <ReadTime>{item.minReadTime}</ReadTime>
+                </Starting>
+                <Title>
+                    {item.title}
+                </Title>
+                <Description>
+                    {item.description}
+                </Description>
+                <Ending>
+                    <Likes>{item.likes}</Likes>
+                    <Comments>{item.comments}</Comments>
+                    {/* <Date>{item.date}</Date> */}
+                </Ending>
+            </PostWrapper>
+            );
+        })}
+        </Wrapper>
+        
     </>
   );
 };
